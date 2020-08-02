a84353c16b0cfc583998e4ca1013de17a333c965
OmegaByte <58628812+OmegaByte@users.noreply.github.com>
2020-08-02 01:05:02 -0400

    Version 1.2
    The plugin deop method has been updated, and a configuration file has been added. It includes NoPermission and Reload messages, and a OpPlayersList for specific Op Players to be deopped.​
    
    Added a config.yml.​
    "/opremover" command has been added.​
    "/opremover reload" command has been added.​

--- src/me/OmegaByte/OpRemover/Main.java
+++ src/me/OmegaByte/OpRemover/Main.java
@@ -2,20 +2,26 @@ package me.OmegaByte.OpRemover;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.List;
+
 public class Main extends JavaPlugin implements Listener {
 
+
     public void onEnable() {
+        new Reload(this);
+            getLogger().info(ChatColor.translateAlternateColorCodes('&', "&8[&cOpRemover&8] &aThe version you are running is outdated, you can download the latest update at https://www.spigotmc.org/resources/opremover.81837/"));
         Bukkit.getPluginManager().registerEvents(this, (Plugin)this);
         Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&c" + getDescription().getName() + "&8] &ahas been enabled."));
         Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&aMade by OmegaByte"));
+        saveDefaultConfig();
     }
 
     public void onDisable() {
@@ -26,9 +32,20 @@ public class Main extends JavaPlugin implements Listener {
     @EventHandler
     public void onQuit(PlayerQuitEvent e) {
         Player p = e.getPlayer();
-        if (p.isOp()) {
-            Bukkit.dispatchCommand((CommandSender) Bukkit.getConsoleSender(), "deop " + p.getName());
-            Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&b&nServer&8: &aOperator status has been removed from " + p.getName() + " after leaving the server."));
+        List<String> OpPlayers = getConfig().getStringList("OpPlayerList");
+        if (p.isOp() && OpPlayers.contains(p.getName())) {
+                p.setOp(false);
+                Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&b&nServer&8: &aOperator status has been removed from " + p.getName() + " after leaving the server."));
+            }
+    }
+
+    @EventHandler
+    public void onShutdown(PlayerKickEvent e) {
+        Player p = e.getPlayer();
+        List<String> OpPlayers = getConfig().getStringList("OpPlayerList");
+        if (p.isOp() && OpPlayers.contains(p.getName())) {
+            p.setOp(false);
+            Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&b&nServer&8: &aOperator status has been removed from " + p.getName() + " after being kicked from the server."));
         }
     }
 }
