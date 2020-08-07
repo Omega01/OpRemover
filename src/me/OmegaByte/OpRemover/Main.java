387cfffc2f8d60815d803d80d207bc20853f9dd6
OmegaByte <58628812+OmegaByte@users.noreply.github.com>
2020-08-07 18:56:38 -0400

    Update Main.java

--- src/me/OmegaByte/OpRemover/Main.java
+++ src/me/OmegaByte/OpRemover/Main.java
@@ -4,8 +4,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -14,10 +14,10 @@ import java.util.List;
 
 public class Main extends JavaPlugin implements Listener {
 
-
     public void onEnable() {
+        int pluginId = 8395;
+        MetricsLite metrics = new MetricsLite(this, pluginId);
         new Reload(this);
-            getLogger().info(ChatColor.translateAlternateColorCodes('&', "&8[&cOpRemover&8] &aThe version you are running is outdated, you can download the latest update at https://www.spigotmc.org/resources/opremover.81837/"));
         Bukkit.getPluginManager().registerEvents(this, (Plugin)this);
         Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&c" + getDescription().getName() + "&8] &ahas been enabled."));
         Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&aMade by OmegaByte"));
@@ -29,23 +29,15 @@ public class Main extends JavaPlugin implements Listener {
         Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&cMade by OmegaByte"));
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onQuit(PlayerQuitEvent e) {
         Player p = e.getPlayer();
         List<String> OpPlayers = getConfig().getStringList("OpPlayerList");
         if (p.isOp() && OpPlayers.contains(p.getName())) {
-                p.setOp(false);
-                Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&b&nServer&8: &aOperator status has been removed from " + p.getName() + " after leaving the server."));
-            }
-    }
-
-    @EventHandler
-    public void onShutdown(PlayerKickEvent e) {
-        Player p = e.getPlayer();
-        List<String> OpPlayers = getConfig().getStringList("OpPlayerList");
-        if (p.isOp() && OpPlayers.contains(p.getName())) {
             p.setOp(false);
-            Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&b&nServer&8: &aOperator status has been removed from " + p.getName() + " after being kicked from the server."));
+            if (getConfig().getBoolean("LogToConsole")) {
+                Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("ConsoleLog").replace("%p", p.getName())));
+            }
         }
     }
 }
